1c7e071e223a9c647ebcad55e5f11b5978c77aa2
Mraedis <chaos.side@gmail.com>
2022-12-02 08:56:03 +0100

    Unoptimize day02 to improve legibility

--- Days/Day02_1.rb
+++ Days/Day02_1.rb
@@ -4,8 +4,9 @@ points1 = 0
 points2 = 0
 for round in rounds
     p1, p2 = round.split(" ")
-    points1 += (p2.ord - p1.ord - 22).modulo(3) * 3 + (p2.ord - 87)
-    points2 += 1 + (p1.ord + p2.ord - 154).modulo(3) + 3 * (p2.ord - 88)
+    # Convert A/B/C and X/Y/Z to their ord and calculate who wins what, add points for used weapon
+    points1 += (p2.ord - 88 - (p1.ord - 66)).modulo(3) * 3 + (p2.ord - 87)
+    points2 += 1 + ((p1.ord - 66) + (p2.ord - 88)).modulo(3) + 3 * (p2.ord - 88)
 end
 puts "Answer to part 1:\n"
 puts points1
