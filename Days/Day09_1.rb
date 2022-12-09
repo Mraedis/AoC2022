cb7551aed46f83346976f9bd09de41d695991e21
Mraedis <chaos.side@gmail.com>
2022-12-09 13:33:36 +0100

    Day09 cleanup and generic

--- Days/Day09_1.rb
+++ Days/Day09_1.rb
@@ -2,51 +2,37 @@ require 'set'
 
 moves = File.read(File.join(File.dirname(__FILE__),  "../Inputs/Input09.txt")).split("\n")
 
-head = [0, 0]
-tail = [0, 0]
-visited1 = Set.new
-visited1 << tail
-for m in moves
-    dir, a = m.split(' ')
-    amount = a.to_i
-    while amount > 0
-        old = head.dup()
-        head[0] += 'LR'.include?(dir) ? 'LR'.index(dir) * 2 - 1 : 0
-        head[1] += 'DU'.include?(dir) ? 'DU'.index(dir) * 2 - 1 : 0
-        if (head[0] - tail[0]).abs() > 1 || (head[1] - tail[1]).abs() > 1
-            tail = old
-        end
-        visited1 << tail.dup()
-        amount -= 1
-    end
-end
+snake1 = Array.new(2) {[0, 0]}
+snake2 = Array.new(10) {[0, 0]}
 
-snake = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-visited2 = Set.new
-visited2 << snake[9].dup()
+visited = [Set.new([0, 0]), Set.new([0, 0])]
+
+snakes = [snake1, snake2]
 for m in moves
     dir, a = m.split(' ')
     amount = a.to_i
     while amount > 0
-        snake[0][0] += 'LR'.include?(dir) ? 'LR'.index(dir) * 2 - 1 : 0
-        snake[0][1] += 'DU'.include?(dir) ? 'DU'.index(dir) * 2 - 1 : 0
-        for i in 1..9
-            dx = snake[i-1][0] - snake[i][0]
-            adx = dx.abs()
-            dy = snake[i-1][1] - snake[i][1]
-            ady = dy.abs()
-            if adx > 1 || ady > 1
-                snake[i][0] += (dx).abs() > 0 ? (dx) / (dx).abs() : 0
-                snake[i][1] += (dy).abs() > 0 ? (dy) / (dy).abs() : 0
+        snakes.each_with_index do |snake,index|
+            snake[0][0] += 'LR'.include?(dir) ? 'LR'.index(dir) * 2 - 1 : 0
+            snake[0][1] += 'DU'.include?(dir) ? 'DU'.index(dir) * 2 - 1 : 0
+            for i in (1...snake.size())
+                dx = snake[i-1][0] - snake[i][0]
+                adx = dx.abs()
+                dy = snake[i-1][1] - snake[i][1]
+                ady = dy.abs()
+                if adx > 1 || ady > 1
+                    snake[i][0] += adx > 0 ? dx / adx : 0
+                    snake[i][1] += ady > 0 ? dy / ady  : 0
+                end
             end
+            visited[index] << snake[-1].dup()
         end
-        visited2 << snake[9].dup()
         amount -= 1
     end
 end
 
 puts "Answer to part 1:\n"
-puts visited1.size()
+puts visited[0].size()
 
 puts "Answer to part 2:\n"
-puts visited2.size()
+puts visited[1].size()
